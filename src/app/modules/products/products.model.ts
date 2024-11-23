4468419a6a5398e5d33af6d3e8beb6650f3e4984
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-11-23 20:42:31 +0600

    product interface and model part done

---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+import { Product } from "./products.interface";
+
+
+const productSchem = new Schema<Product>({
+    name: {type : String, required: true},
+    brand : {type: String, required: true},
+    price : {type: Number, required: true},
+    category : {
+        type : String,
+        enum: ["Mountain" , "Road" ,  "Hybrid" , "Electric"]
+    },
+    description : {type: String, required: true},
+    quantity : {type: Number, required: true},
+    inStock : {type: Boolean, required: true}
+});
+
+
+ export const ProductModel = model<Product>('ProductModel', productSchem);
