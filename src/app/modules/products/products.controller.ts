d20c6e5b92ca032298e98598d3283e1191a6b18e
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-11-24 13:02:58 +0600

    zod validation added

--- src/app/modules/products/products.controller.ts
+++ src/app/modules/products/products.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import { productService } from './products.service';
+import productValidationSchema from './products.validation';
 
 const createProduct = async (req: Request, res: Response) => {
   try {
     const product = req.body;
+    const validatedProduct = productValidationSchema.parse(product);
 
-    const result = await productService.createProductIntoDB(product);
+    const result = await productService.createProductIntoDB(validatedProduct);
 
     res.status(200).json({
       success: true,
