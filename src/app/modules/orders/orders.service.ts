cd001210658b2be258c848a2c1c0486850d6478e
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-11-24 23:12:19 +0600

    oder create part updated

--- src/app/modules/orders/orders.service.ts
+++ src/app/modules/orders/orders.service.ts
@@ -1,8 +1,13 @@
+import mongoose from "mongoose";
+import { ProductModel } from "../products/products.model";
 import { Order } from "./orders.interface";
 import { OrderModel } from "./orders.model";
 
 const CreateOderIntoDB = async(order: Order) => {
     const result = await OrderModel.create(order);
+    await ProductModel.findByIdAndUpdate({_id : new mongoose.Types.ObjectId(order.product)}, {
+        $inc : {quantity : -order.quantity}
+    });
     return result;
 }
 
